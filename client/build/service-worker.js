d62a38c2073df988a2c9c0ed01b42bb43cae6ba2
Javlonbek <javlonarjei042@gmail.com>
2020-02-20 16:53:28 +0500

    build

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c9aa0c37758ad86f6faddd31eba7baef.js"
+  "/precache-manifest.67d1a6cf3039cb44db201c4b44462e1d.js"
 );
 
 self.addEventListener('message', (event) => {
